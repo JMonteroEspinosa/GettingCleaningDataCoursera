290e01a779929d8002829fbc1a3fa13cde8b726c
Juan Montero <jmonteroers@gmail.com>
2020-03-24 18:39:18 +0100

    Finished last steps

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GettingCleaningDataCoursera
 Assignment Week 4
+
+
+Change of variable names
+Jerk stands on its own
+https://en.wikipedia.org/wiki/Jerk_(physics)
--- step2.R
+++ step2.R
@@ -19,12 +19,44 @@ names(df)[3:dim(df)[2]] <- feature_names
 
 # remove duplicated features
 df <- df[,!duplicated(names(df))]
-# select only means and stds
+# select only means and stds AND subject, target
 df <- df %>%
-  select(contains(c("std", "mean")))
+  select( "subject" | "activity" | contains(c("std", "mean"))) %>%
+  # remove mean frequency
+  select(-contains("meanFreq"))
 
 # STEP 3: rename activities
+# grab activities
+# activities is a lookup-table
+activities <- read.table("UCI HAR Dataset/activity_labels.txt",
+                            stringsAsFactors = F)
+# V1 equals current index, can drop it
+activities <- activities$V2
+print(activities[4])
 
-
+# label target
+df$activity <- factor(df$activity, labels = activities)
 
 # STEP 4: rename variables
+# create a function that applies all the transformations
+clean_vars <- function(var) {
+  # transformation 1: Mag -> Total
+  var <- gsub("Mag", "Total", var)
+  # transformation 2: initial t -> empty
+  var <- gsub("^t", "", var)
+  # transformation 3: remove empty parentheses
+  var <- gsub("\\(\\)", "", var)
+  # transformation: - -> _
+  var <- gsub("-", "_", var)
+  # transformation: Gyro -> AngVelocity
+  var <- gsub("Gyro", "AngVelocity", var)
+  # transformation: initial f -> FFT_ 
+  # FFT stands for Fast Fourier Transform
+  var <- gsub("^f", "FFT_", var)
+  return(var)
+}
+
+names(df) <- sapply(names(df), clean_vars)
+
+
+
